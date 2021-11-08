87e6d4692d26b2de29b8660ef758321810091768
Victor Leta <victor.rl.oliveira@gmail.com>
2021-11-07 21:47:39 -0300

    Finalização do projeto.
    A responsividade vai ficar para depois por falta de tempo. Ainda quebra em algumas resoluções, eu acho.
    
    Eu também queria deixar mais ricas as respostas com imagens etc, mas por falta de tempo tive que deixar para depois também.

---
+++ kafra.js
@@ -0,0 +1,31 @@
+// buscar os itens da tela
+const itensPerguntasERespostas = document.querySelectorAll('.item-do-quadro');
+
+// console.log(itensPerguntasERespostas);
+
+// entender que o item foi clicado
+itensPerguntasERespostas.forEach(function(item){
+    // console.log(item)]
+    item.addEventListener('click', function(){
+        // console.log('clicou!');
+        // verificar se a pergunta clicada está ativa
+        const estaAtiva = item.classList.contains('ativo')
+        // se a pergunta não estiver ativa
+        if(!estaAtiva){
+            // fechar todas as outras
+            itensPerguntasERespostas.forEach(function(item){
+                item.classList.remove('ativo')
+            })
+            // abrir a resposta atual
+            item.classList.add('ativo')
+        }
+            
+    
+        // se a pergunta estiver ativa
+
+            // fechar a resposta atual(remover a classe ativo)
+    })
+})
+
+
+    
