0fbf63a62d1cf325dfc6e606cfa3ea01ec1509e5
Victor Leta <victor.rl.oliveira@gmail.com>
2021-11-23 15:53:39 -0300

    Aprendendo .gitignore

--- kafra.js
+++ kafra.js
@@ -5,18 +5,23 @@ const itensPerguntasERespostas = document.querySelectorAll('.item-do-quadro');
 
 // entender que o item foi clicado
 itensPerguntasERespostas.forEach(function(item){
+
     // console.log(item)
     item.addEventListener('click', function(){
+
         // console.log('clicou!');
         // verificar se a pergunta clicada está ativa
         const estaAtiva = item.classList.contains('ativo')
+
         // se a pergunta não estiver ativa
         if(!estaAtiva){
+
             // fechar todas as outras
             itensPerguntasERespostas.forEach(function(item){
                 item.classList.remove('ativo')
                 item.classList.add('escondido')
             })
+            
             // abrir a resposta atual
             item.classList.add('ativo')
             item.classList.remove('escondido')
@@ -29,6 +34,7 @@ itensPerguntasERespostas.forEach(function(item){
                 item.classList.remove('ativo')
                 item.classList.remove('escondido')
             })
+        
             // fechar a resposta atual(remover a classe ativo)
             item.classList.remove('ativo')
         }
